3f24941c86d68405932e96e247b6e67ee4233e4b
Anton Usoltsev <usoltsevjr@gmail.com>
2020-07-31 10:42:03 +0300

    add 2nd test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,4 +18,15 @@ class StatisticsServiceTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void anotherMax() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+
+}
 }
